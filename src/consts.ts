a2de0d0b4f2ff68889ef0e5e08922f6d02f84c7f
rgiusti <rgiusti@alabamasolutions.com>
2023-04-10 21:52:01 -0300

    social and desc updated.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Alabama Solutions | Software Development';
+export const SITE_DESCRIPTION = 'We are your digital transformation partners for Digital Transformation. Mobile Apps, Web & Emerging Technologies.';
